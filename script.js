9977511a7b2d8654ac64b32b9d5c6bfcf5f6497d
Lucas Antunes de Lara <lucasantuneslara06@gmail.com>
2022-10-12 12:57:17 -0300

    js melhorado

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function $(id) {
+  return document.getElementById(id);
+}
+
+let codigoCandidatoSelecionado;
+let nomeCandidatoSelecionado;
+let partidoCandidatoSelecionado;
+let fotoCandidatoSelecionado;
+
 let codigoCandidato1 = 10
 let codigoCandidato2 = 06
 let codigoCandidato3 = 79
@@ -10,183 +19,87 @@ let nomePartidoCandidato1 = "Libertadores"
 let nomePartidoCandidato2 = "Mundial"
 let nomePartidoCandidato3 = "Brasileirão"
 
-let fotoCandidato1 = Dale.png
-let fotoCandidato2 = Fernandao.png
-let fotoCandidato3 = Falcao.png
+let fotoCandidato1 = "Dale.png";
+let fotoCandidato2 = "Fernandao.png"
+let fotoCandidato3 = "Falcao.png"
 
+let candidatoExiste = false;
+let fim = $("fim")
 
 function inserir(valor) {
-  let num1 = document.getElementById("num1").value
-  let num2 = document.getElementById("num2").value
-  const hidden = document.getElementById("valorEscondido")
- 
+  let num1 = $("num1").value
+  let num2 = $("num2").value
+
   if (num1 == "") {
-      document.getElementById("num1").value = valor;
-      document.getElementById("num1").style.border = "solid 2px gray"
-  } else if (num2 == "") {
-      document.getElementById("num2").value = valor
-      document.getElementById("num1").style.border = "solid 1px black"
-      document.getElementById("num2").style.border = "solid 2px gray"
+    $("num1").value = valor;
+    $("num1").style.border = "solid 2px gray"
+  } else if (num2 == "" || num1 != "") {
+    $("num2").value = valor
+    $("num1").style.border = "solid 1px black"
+    $("num2").style.border = "solid 2px gray"
   }
-  
-  var valor1 = parseInt(document.getElementById("num1").value);
-  var valor2 = parseInt(document.getElementById("num2").value);
-  let candidado = (valor1 * 10) + valor2;
 
-  hidden.innerHTML = candidado
+  var valor1 = parseInt($("num1").value);
+  var valor2 = parseInt($("num2").value);
+  let candidato = (valor1 * 10) + valor2;
 
-  if(candidado === codigoCandidato1){
-    document.getElementById('foto').src = 'Dale.png'
-    document.getElementById('foto').style.visibility = "visible"
-    //elementoOculto.style.visibility = "visible"
-    document.getElementById('nomeCandidato').innerHTML = nomeCandidato1
-    // document.getElementById('numPartido').innerHTML = codigoCandidato1
-    document.getElementById('partido').innerHTML = nomePartidoCandidato1
-  }
-  else if(candidado === codigoCandidato2){
-    document.getElementById('foto').src = 'Fernandao.png'
-    document.getElementById('foto').style.visibility = "visible"
-    document.getElementById('nomeCandidato').innerHTML = nomeCandidato2
-    // document.getElementById('numPartido').innerHTML = 06
-    document.getElementById('partido').innerHTML = nomePartidoCandidato2
-  }
-  else if(candidado === codigoCandidato3){
-    document.getElementById('foto').src = 'Falcao.png'
-    document.getElementById('foto').style.visibility = "visible"
-    document.getElementById('nomeCandidato').innerHTML = nomeCandidato3
-    // document.getElementById('numPartido').innerHTML = codigoCandidato3
-    document.getElementById('partido').innerHTML = nomePartidoCandidato3
+  switch (candidato) {
+    case codigoCandidato1:
+      $('foto').src = fotoCandidato1
+      $('foto').style.visibility = 'visible'
+      $('nomeCandidato').innerHTML = nomeCandidato1
+      $('partido').innerHTML = nomePartidoCandidato1
+      candidatoExiste = true;
+
+      break;
+
+    case codigoCandidato2:
+      $('foto').src = fotoCandidato2
+      $('foto').style.visibility = "visible"
+      $('nomeCandidato').innerHTML = nomeCandidato2
+      $('partido').innerHTML = nomePartidoCandidato2
+      candidatoExiste = true;
+      break;
+
+
+    case codigoCandidato3:
+      $('foto').src = fotoCandidato3
+      $('foto').style.visibility = "visible"
+      $('nomeCandidato').innerHTML = nomeCandidato3
+      $('partido').innerHTML = nomePartidoCandidato3
+      candidatoExiste = true;
+      break;
+
+    default:
+      codigoCandidatoSelecionado = undefined
+      nomeCandidatoSelecionado = undefined
+      partidoCandidatoSelecionado = undefined
+      candidatoExiste = false;
   }
 }
 
 function corrige() {
-  document.getElementById("num1").value = ""
-  document.getElementById("num2").value = ""
-  document.getElementById("foto").src = ""
-  document.getElementById("nomeCandidato").innerHTML = ""
-  document.getElementById("numPartido").innerHTML = ""
-  document.getElementById("partido").innerHTML = ""
-  document.getElementById('foto').style.visibility = 'hidden'
+  $("num1").value = ""
+  $("num2").value = ""
+  $("foto").src = ""
+  $("nomeCandidato").innerHTML = ""
+  $("partido").innerHTML = ""
+  $('foto').style.visibility = 'hidden'
+  $('imgCandidato').style.visibility = 'hidden'
 }
-
-let codigoCandidatoSelecionado = null
-let nomeCandidatoSelecionado = document.getElementsByTagName(image).src = ""
-let partidoCandidatoSelecionado = null
-let fotoCandidatoSelecionado = null
 
 function votar() {
- // console.log(candidato)
-  //console.log(document.getElementById("valorEscondido").innerHTML)
-  //console.log(valor2)
-  let votoConfirmado = document.getElementById('esquerda')
-  if(document.getElementById("valorEscondido").innerHTML == 79 || document.getElementById("valorEscondido").innerHTML == 06 || document.getElementById("valorEscondido").innerHTML == 10){
-    esquerda.innerHTML = "Voto Confirmado"
+  let votoConfirmado = $('esquerda')
+  if (candidatoExiste === true) {
+    $('infosCandidato').style.display = "none"
+    fim.style.display = "flex"
+    $('foto').style.display = 'none'
   }
-  }
-
-function votoBranco(){
- if(esquerda.innerHTML == "Voto Confirmado"){
-  esquerda.innerHTML = "Voto anulado"}
 }
-
-// default:{
-//     codigo
-// }
-// switch(codigo){
-  
-
-//   case codigoCandidato1://{
-//     codigoCandidatoSelecionado = codigoCandidato1
-//     nomeCandidatoSelecionado = nomeCandidato1
-//     partidoCandidatoSelecionado = partidoCandidato1
-
-//     nome.innerHTML = nomeCandidato1
-
-//     break;
-//  // }
-//   case codigoCandidato2://{
-//     codigoCandidatoSelecionado = codigoCandidato2
-//     nomeCandidatoSelecionado = nomeCandidato2
-//     partidoCandidatoSelecionado = partidoCandidato2
-//     break;
-//  // }
-//   case codigoCandidato1://{
-//     codigoCandidatoSelecionado = codigoCandidato3
-//     nomeCandidatoSelecionado = nomeCandidato3
-//     partidoCandidatoSelecionado = partidoCandidato3
-//     break;
-//   //}
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // const btn1 = document.getElementsById('btn1')
-  // const btn2 = document.getElementsById('btn2')
-  // const btn3 = document.getElementsById('btn3')
-  // const btn4 = document.getElementsById('btn4')
-  // const btn5 = document.getElementsById('btn5')
-  // const btn6 = document.getElementsById('btn6')
-  // const btn7 = document.getElementsById('btn7')
-  // const btn8 = document.getElementsById('btn8')
-  // const btn9 = document.getElementsById('btn9')
-  // const btn0 = document.getElementsById('btn0')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//console.log(numPartido.value)
-
-
-    // switch(codigo){
-    //     case codigoCandidatoSelecionado === 13
-    //     :codigoCandidatoSelecionado = codigoCandidato1
-    //     input.value === codigoCandidato1
-
-    // }
-
+function votoBranco() {
+  if (fim.style.display = "none") {
+    $('infosCandidato').style.display = "none"
+    fim.style.display = "flex"
+    $('foto').style.display = 'none'
+  }
+}
